82898d899ad4ef95a9c0bae41acb14b54eeca03a
MohamedGOLD111 <mohamedgold1612@gmail.com>
2026-07-22 18:31:28 +0300

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -1,1 +1,19 @@
+import os
+from groq import Groq
 
+
+class GroqTranscriber:
+    def __init__(self, api_key):
+        self.client = Groq(api_key=api_key)
+
+    def transcribe(self, audio_path):
+        with open(audio_path, "rb") as audio_file:
+            response = self.client.audio.transcriptions.create(
+                file=audio_file,
+                model="whisper-large-v3-turbo",
+                response_format="verbose_json",
+                language="en",
+                temperature=0
+            )
+
+        return response
